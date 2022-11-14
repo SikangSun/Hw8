3c461ddcabfea6a549a83e00b9709a548c6b3bee
SikangSun <89489927+SikangSun@users.noreply.github.com>
2022-11-13 20:59:30 -0500

    asdfasdf

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,9 +1,44 @@
 import express from "express";
 const router = express.Router();
+import { MongoClient } from 'mongodb'
+router.use(express.json());
+
+
+
+
+
+
 
 /* GET home page. */
 router.get("/", (req, res, next) => {
   return res.send({hello: "world"});
+});
+router.post("/create", (req, res, next) => {
+  const obj = req.body
+  console.log(obj)
+  MongoClient.connect(process.env.MONGODB_URL, (err, db) => {
+    if (err) throw err;
+    const dbo = db.db("hw8");
+    dbo.collection("blog").insertOne(obj, function(err, res) {
+      if (err) throw err;
+      console.log("1 document inserted");
+      db.close();
+    })
+  })
+});
+
+router.get("/feed", async (req, res, next) => {
+  const obj = req.body
+  let returnobj = [];
+  console.log(returnobj)
+  await MongoClient.connect(process.env.MONGODB_URL, async (err, db) => {
+    if (err) throw err;
+    const dbo = db.db("hw8");
+    console.log("in");
+    await dbo.collection("blog").find().forEach(ob => returnobj.push(ob))
+    res.send(returnobj)
+  })
+
 });
 
 export default router;
--- frontend/src/pages/index.jsx
+++ frontend/src/pages/index.jsx
@@ -1,5 +1,89 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 export function Index() {
-  return <div>
-    Index
-  </div>;
+  const url = "http://localhost:3000";
+  const [showCreation, setShowCreation] = useState(false);
+  const [showFeed, setShowFeed] = useState(false);
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");  
+  const [json, setJson] = useState([]);
+
+  useEffect(() => {
+    const request = async () => {
+    const requestOptions = {
+      headers:  { 'Content-Type':'application/json'}
+    }
+    const res = await axios.get(`${url}/feed`,requestOptions)
+    setJson(res.data);
+  }
+  request();
+  
+  }, [])
+  
+  const createComp = () => {
+    const handleSubmit = async (e) => {
+      e.preventDefault();
+      const obj = {
+        "title":title,
+        "content":content
+      }
+      const requestOptions = {
+        headers:  { 'Content-Type':'application/json'}
+      }
+      const res = await axios.post(`${url}/create`, obj, requestOptions);
+      setShowCreation(false);
+    }
+    return (
+      <>
+        <div>
+          Title:<textarea value={title} cols="30" rows="1" onChange={(e) => setTitle(e.target.value)}></textarea>
+        </div>
+        <div>
+          Content:
+        </div>
+          <textarea value={content} cols="30" rows="10" onChange={(e) => setContent(e.target.value)}></textarea>
+        <button onClick={(e) => handleSubmit(e)}>Submit</button>
+      </>
+    )
+  }
+  const feedComp = () => {
+
+    return (
+      <> 
+      Blog Feed: <hr/>
+      <div>
+        {json.map((obj,index) => {
+          return (
+          <div> 
+            <div>
+            {obj.title}
+            </div>
+            <div>
+            {obj.content}
+            </div>
+            <hr/>
+          </div>
+        )})}
+      </div>
+      </>
+    )
+  }
+  const handleCreation = (e) => {
+    e.preventDefault();
+    setShowFeed(false);
+    setShowCreation(true);
+  }
+  const handleFeed = async (e) => {
+    e.preventDefault();
+    setShowCreation(false);
+    setShowFeed(true)
+
+  }
+
+  return (<div>
+    <button onClick={(e) => handleCreation(e)}>Create</button><br/>
+    <button onClick= {(e) => handleFeed(e)}>Show Blog</button><br/>
+    {showCreation ? createComp(): ""}
+    {showFeed ? feedComp() : ""}
+  </div>);
 }
